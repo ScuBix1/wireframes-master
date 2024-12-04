9dfbd0af9938b1c9b3b71d87651451118e532c3b
ScuBix1 <bastian.monnin1@gmail.com>
2024-12-04 23:20:50 +0100

    Création de l'application

---
+++ public/js/index.js
@@ -0,0 +1,138 @@
+const topics = [
+  'visual-hierarchy',
+  'interaction-design',
+  'wireframe-fidelity',
+  'accessibility-inclusion',
+];
+
+const roomNumber = window.location.href.split('/')[3];
+
+const socket = io();
+
+const baseUrl = "http://192.168.65.126"
+const port = 3000
+
+if (document.getElementById('saveUsername')) {
+  const usernameForm = document.getElementById('saveUsername');
+  if (document.getElementById('username')) {
+    const username = document.getElementById('username');
+    usernameForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      player = {
+        role: 'player',
+        username: username.value,
+        socketId: socket.id,
+        roomId: roomNumber,
+        win: false,
+      };
+      socket.emit('playerData', player, roomNumber);
+      window.location.href = `${baseUrl}:${port}/${roomNumber}/${player.username}/${player.role}`;
+    });
+  }
+}
+
+if (document.getElementById('players')) {
+  const playersList = document.getElementById('players');
+  socket.emit('getPlayersRoomById', roomNumber);
+  socket.on('listPlayers', (players) => {
+    let html = '';
+    players.map((player) => {
+      html += `<li>${player.username}</li>`;
+    });
+    playersList.innerHTML = html;
+  });
+}
+
+if (document.getElementById('spinButton')) {
+  const spinButton = document.getElementById('spinButton');
+  player = {
+    role: 'player',
+    username: window.location.href.split('/')[4],
+    socketId: socket.id,
+    roomId: roomNumber,
+    win: false,
+  };
+  spinButton.addEventListener('click', () => {
+    socket.emit('spinWheel');
+  });
+  socket.on('updateWheel', (data, choiceTopic) => {
+    const { angle } = data;
+    const wheel = document.getElementById('wheel');
+    if (wheel) {
+      wheel.style.transform = `rotate(${angle}deg)`;
+    }
+    setTimeout(() => {
+      window.location.href = `${baseUrl}:${port}/${roomNumber}/${player.username}/${player.role}/${choiceTopic}`;
+    }, 6500);
+  });
+}
+
+if (document.getElementById('form-container')) {
+  socket.emit(
+    'selectQuestion',
+    window.location.href.split('/')[3],
+    window.location.href.split('/')[4],
+    window.location.href.split('/')[6]
+  );
+  socket.on('sendQuestion', (question) => {
+    const container = document.getElementById('form-container');
+    container.innerHTML = '';
+    const questionElement = document.createElement('h3');
+    questionElement.textContent = question.question;
+    container.appendChild(questionElement);
+    const form = document.createElement('form');
+    form.id = 'quiz-form';
+    question.answers.forEach((answer, index) => {
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox'; // Utilisation de checkbox
+      checkbox.name = 'answer'; // Même nom pour que ce soient des cases à cocher liées
+      checkbox.value = answer;
+      checkbox.id = `answer${index}`; // Id unique pour chaque case à cocher
+
+      const label = document.createElement('label');
+      label.setAttribute('for', checkbox.id); // Lier le label à la checkbox
+      label.textContent = answer;
+
+      form.appendChild(checkbox);
+      form.appendChild(label);
+      form.appendChild(document.createElement('br'));
+
+      // Ajouter un gestionnaire d'événements pour gérer l'unicité de la sélection
+      checkbox.addEventListener('change', (e) => {
+        if (e.target.checked) {
+          // Désélectionner toutes les autres cases
+          const checkboxes = form.querySelectorAll('input[type="checkbox"]');
+          checkboxes.forEach((otherCheckbox) => {
+            if (otherCheckbox !== e.target) {
+              otherCheckbox.checked = false; // Désélectionner les autres cases
+            }
+          });
+        }
+      });
+    });
+    const submitButton = document.createElement('button');
+    submitButton.type = 'button';
+    submitButton.textContent = 'Submit';
+    submitButton.addEventListener('click', (e) => {
+      e.preventDefault();
+      const checkboxes = form.querySelectorAll('input[type="checkbox"]');
+      checkboxes.forEach((checkbox) => {
+        if (checkbox.checked) {
+          console.log(checkbox.value);
+          socket.emit(
+            'submitAnswer',
+            window.location.href.split('/')[4],
+            window.location.href.split('/')[6],
+            checkbox.value
+          );
+          
+        }
+        socket.on('gameMessage', (message) => {
+            alert(message);
+          });
+      });
+    });
+    form.appendChild(submitButton);
+    container.appendChild(form);
+  });
+}
---
+++ server.js
@@ -0,0 +1,126 @@
+require('dotenv').config(); 
+const { Socket } = require('socket.io');
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+
+const apiRoutes = require('./routes/api');
+/**
+ * @type {Socket}
+ */
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 5000;
+
+app.use('/', apiRoutes);
+app.use(express.static('public'));
+
+http.listen(PORT, () => {
+  console.log(`Serveur lancé sur ${process.env.BASE_URL}:${PORT}`);
+});
+const question = Math.floor(Math.random() * 2);
+let rooms = [];
+const themes = [
+  [
+    {
+      question: 'What is visual hierarchy?',
+      answers: ['A', 'B', 'C', 'D'],
+      correct: 'A',
+    },
+    {
+      question: 'Define contrast.',
+      answers: ['A', 'B', 'C', 'D'],
+      correct: 'B',
+    },
+  ],
+  [
+    {
+      question: 'What is a microinteraction?',
+      answers: ['A', 'B', 'C', 'D'],
+      correct: 'C',
+    },
+    {
+      question: 'Define UX design.',
+      answers: ['A', 'B', 'C', 'D'],
+      correct: 'D',
+    },
+  ],
+  [
+    {
+      question: 'What is low-fidelity wireframe?',
+      answers: ['A', 'B', 'C', 'D'],
+      correct: 'A',
+    },
+    {
+      question: 'Explain high-fidelity wireframe.',
+      answers: ['A', 'B', 'C', 'D'],
+      correct: 'B',
+    },
+  ],
+  [
+    { question: 'What is WCAG?', answers: ['A', 'B', 'C', 'D'], correct: 'C' },
+    {
+      question: 'Define inclusive design.',
+      answers: ['A', 'B', 'C', 'D'],
+      correct: 'D',
+    },
+  ],
+];
+
+io.on('connect', (socket) => {
+  socket.on('playerData', (player, roomId) => {
+    if (rooms.length === 0) {
+      player.role = 'admin';
+      rooms.push({ id: roomId, players: [player] });
+    } else {
+      let cpt = 0;
+      rooms.map((room) => {
+        room.id === roomId ? cpt++ : cpt;
+      });
+      if (cpt === 0) {
+        player.role = 'admin';
+        rooms.push({ id: roomId, players: [player] });
+      } else {
+        rooms.map((room) => {
+          if (room.id === roomId) {
+            room.players.push(player);
+          }
+        });
+      }
+    }
+    rooms.map((room) => {
+      if (
+        room.players.length >= 2 &&
+        room.players.length <= 6 &&
+        room.id === roomId
+      ) {
+        io.to(room.id).emit('startSpin', room.players);
+      }
+    });
+  });
+
+  socket.on('getPlayersRoomById', (idRoom) => {
+    if (rooms.length > 0) {
+      rooms.map((room) => {
+        if (room.id == idRoom) {
+          io.emit('listPlayers', room.players);
+        }
+      });
+    }
+  });
+
+  socket.on('spinWheel', () => {
+    const choiceTopic = Math.floor(Math.random() * 4);
+    io.emit('updateWheel', { angle: choiceTopic * 90 + 1440 }, choiceTopic);
+  });
+  socket.on('selectQuestion', (roomId, username, subject) => {
+    
+    io.emit('sendQuestion', themes[subject][question]);
+  });
+  socket.on('submitAnswer', ( username, subject, answer ) => {
+      const currentQuestion = themes[subject][question];
+      console.log(currentQuestion)
+      if (currentQuestion.correct === answer) {
+        io.emit('gameMessage', `${username} a gagné`);
+      }
+  });
+});
